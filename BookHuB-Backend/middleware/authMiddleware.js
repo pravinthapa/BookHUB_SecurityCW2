3c97ba1612af8d45b878f60424c3d28751e12046
pravinthapa <prabinthapaparbin33@gmail.com>
2025-07-31 22:19:34 +0545

    feat:changes on middleware

---
+++ BookHuB-Backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const authMiddleware = async (req, res, next) => {
+  let token;
+
+
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  if (!token && req.header("Authorization")) {
+    token = req.header("Authorization").split(" ")[1];
+  }
+
+  if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) return res.status(401).json({ msg: "User not found" });
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
+
+module.exports = authMiddleware;
